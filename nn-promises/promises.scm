c2be00f85ba0770d3c721aee0855097c5a4a872d
Michele Lindroos <michele.lindroos@omoroi.fi>
2021-12-08 22:52:51 +0200

    nn-promises: print results

--- nn-promises/promises.scm
+++ nn-promises/promises.scm
@@ -4,10 +4,12 @@
 (display "Hello from script!") (newline) (newline)
 
 (define my-promise (delay (system "echo hello")))
+(define my-promise-exit-1 (delay (system "exit 1")))
 
-(display "Promise is created, let's run it") (newline) (newline)
+(display "Promises created, let's run them") (newline) (newline)
 
-(force my-promise)
+(display "Promise result: ") (display (force my-promise)) (newline) (newline)
 
-(display "Promise was run") (newline) (newline)
+(display "Promise result: ") (display (force my-promise-exit-1)) (newline) (newline)
 
+;;; prints 256 instead of 1???
